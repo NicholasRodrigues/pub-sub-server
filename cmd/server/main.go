f98487eacb2a1e0cbc76fae3f52da797008b68b2
NicholasRodrigues <nicholasgabriel65@gmail.com>
2024-06-10 09:14:24 -0300

    feat: connection with RabbitMQ established

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"os"
+	"os/signal"
+)
+
+const rabbitConnString = "amqp://guest:guest@localhost:5672/"
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	conn, err := amqp.Dial(rabbitConnString)
+	if err != nil {
+		fmt.Printf("new server: %v", err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+	fmt.Printf("Connected to RabbitMQ")
+
+	// wait for ctrl+c
+	signalChanel := make(chan os.Signal, 1)
+	signal.Notify(signalChanel, os.Interrupt)
+	<-signalChanel
 }
